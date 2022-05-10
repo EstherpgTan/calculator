e96b5e1db9394b2653968be35172bbcf95848501
Esther Tan <EstherpgTan@gmail.com>
2022-05-10 21:18:05 +1000

    Update

--- calculator.js
+++ calculator.js
@@ -1,29 +1,4 @@
-// Find the buttons & storing them
-const buttons = document.querySelectorAll(".num-button");
-
-const allButtons = document.querySelectorAll(".button");
-
-const displayBox = document.querySelector("display-box");
-
-const equals = document.querySelector("#equals");
-
-// Storing numbers
-const numButton = [...buttons];
-const allButtonslist = [...allButtons];
-
-// buttonNumbers.map(buttons, i) => {
-for (let i = 0; i < numButton.length; i++) {
-    numButton[i].addEventListener("click", () => inputNumber(this.innerText));
-}
-
-// Operator buttons
-for (let i = 0; i < allButtonslist.length; i++) {
-    allButtonslist[i].addEventListener("click", () =>
-        allButtonslist(this.innerText)
-    );
-}
-
-// Use of operations with numbers
+// Returning numbers
 let currentNumber = "";
 
 const inputNumber = (anyNumber) => {
@@ -31,6 +6,35 @@ const inputNumber = (anyNumber) => {
     display();
 };
 
+// Display on calculator
+const display = () => {
+    document.getElementById("display").innerText = currentNumber;
+};
+
+// Numbers
+let numButton = [
+    "num-button-0",
+    "num-button-1",
+    "num-button-2",
+    "num-button-3",
+    "num-button-4",
+    "num-button-5",
+    "num-button-6",
+    "num-button-7",
+    "num-button-8",
+    "num-button-9",
+    "num-button-.",
+];
+
+for (let i = 0; i < numButton.length; i++) {
+    let number = numButton[i];
+
+    let buttons = document.getElementById(number);
+
+    buttons.addEventListener("click", () => inputNumber(buttons.innerText));
+}
+
+// Defines previous and current number with an operator event
 let previousNumber = "";
 let currentOperator = "";
 
@@ -40,19 +44,33 @@ const inputOperator = (op) => {
     currentNumber = "";
 };
 
+// Operator buttons
+let operatorButton = ["÷", "x", "-", "+"];
+
+for (let i = 0; i < operatorButton.length; i++) {
+    let operation = operatorButton[i];
+
+    let operator = document.getElementById("operator-" + operation);
+
+    operator.addEventListener("click", () => inputOperator(operator.innerText));
+}
+
 const equalsButton = () => {
     if (currentOperator === "÷") {
         currentNumber = Number(previousNumber) / Number(currentNumber);
         display();
     }
+
     if (currentOperator === "x") {
         currentNumber = Number(currentNumber) * Number(previousNumber);
         display();
     }
+
     if (currentOperator === "-") {
         currentNumber = Number(previousNumber) - Number(currentNumber);
         display();
     }
+
     if (currentOperator === "+") {
         currentNumber = Number(currentNumber) + Number(previousNumber);
         display();
@@ -60,10 +78,5 @@ const equalsButton = () => {
 };
 
 // Equals: Total
-const totalsButton = document.getElementById("equals");
-totalsButton.addEventListener("click", () => totalsButton());
-
-// Display
-const display = () => {
-    document.getElementById("display-box").innerText = currentNumber;
-};
+const equals = document.getElementById("equals");
+equals.addEventListener("click", () => equalsButton());
